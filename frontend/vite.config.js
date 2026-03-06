4c195c60ad81895b291f54563f198c6335fb8651
Sanket Kadam <sk8185710@gmail.com>
2026-03-06 15:07:44 +0530

    change vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // allows Vite to accept requests from any host
+    // optional: explicitly allow your deployed host
+    allowedHosts: ['notestask-frontend.onrender.com'],
+  },
 })
